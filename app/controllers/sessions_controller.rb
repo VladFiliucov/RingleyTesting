24badd8135c7b4abea0c21b44732ef9d35b25a4c
Vlad Filiucov <filiucov@mail.ru>
2015-07-15 12:59:21 +0100

    Remove Upload new doc link form normal users's view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(login: params[:session][:login].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to documents_path
     else
       render 'new'
     end
