82af05652ad6a438cf0f98ac74fb8313dfed7118
Vlad Filiucov <filiucov@mail.ru>
2015-07-15 12:04:02 +0100

    Add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:login, :password,
+                                   :password_confirmation)
   end
 end
